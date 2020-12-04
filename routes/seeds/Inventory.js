d297d456f2b5689ea7e5b53c7ec854500eafc17d
Siyabonga Msindwana <34550203+SXMSINDWANA@users.noreply.github.com>
2020-12-04 10:43:32 +0200

    DB connection removed

--- routes/seeds/Inventory.js
+++ routes/seeds/Inventory.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 
 
 //connect to db
-mongoose.connect('mongodb+srv://dbUser:P@$$Word@cluster0.qdyp3.mongodb.net/tst_dbQE?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-    console.log('connected to DB');
-    // const collection = client.db("test").collection("devices");
-    // console.log(collection);
-})
-
 
 const Inventory = mongoose.model('InventoryItem', InventoryItemSchema);
 
--- routes/seeds/Menus.js
+++ routes/seeds/Menus.js
@@ -1,9 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dbUser:P@$$Word@cluster0.qdyp3.mongodb.net/tst_dbQE?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-    console.log('connected to DB');
-    // const collection = client.db("test").collection("devices");
-    // console.log(collection);
-})
 
 const MenuCatgorySchema = mongoose.Schema({
     name: String,
