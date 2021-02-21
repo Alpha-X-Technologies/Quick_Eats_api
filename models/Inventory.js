a0d6de43502e90521cff065b62a6460e6266175f
Siba <sibajikani@gmail.com>
2021-02-22 00:38:49 +0200

    More clean up

--- models/Inventory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const InventoryItemSchema = mongoose.Schema({
-    name: String
-});
-
-
-module.exports = mongoose.model('InventoryItem', InventoryItemSchema);
